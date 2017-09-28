606dc73473d9a6547178baf62029c10cd5d63867
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-28 10:19:15 -0300

    add comentário variáveis locais

--- src/programmer/escopo/variaveis/VariaveisLocais.java
+++ src/programmer/escopo/variaveis/VariaveisLocais.java
@@ -3,12 +3,18 @@ package programmer.escopo.variaveis;
 public class VariaveisLocais {
 	static int valor = 10;
 
+	/*
+	 * exemplo de utilização de variável local
+	 */
 	public static void main(String[] args) {
 		int y = 0;
 		int x = VariaveisLocais.valor;
 		if (x >= 10) {
 			y = x / 10;
 		} else {
+			// Se a declaração da variável y fosse no bloco if, ela
+			// não poderia ser chamada fora dessa condição
+			// int y
 			y = x * 5;
 		}
 		System.out.println(y);
