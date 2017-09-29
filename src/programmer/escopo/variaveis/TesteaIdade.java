1c4e51ff014f1e297689289dfcccb872b5a3ab36
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-29 15:38:48 -0300

    add classe para manipulação simples de variável

---
+++ src/programmer/escopo/variaveis/TesteaIdade.java
@@ -0,0 +1,15 @@
+package programmer.escopo.variaveis;
+
+public class TesteaIdade {
+
+	public static void main(String[] args) {
+		int idade = 29;
+		System.out.println(idade);
+
+		int idadeNoAnoQueVem;
+		idadeNoAnoQueVem = idade + 1;
+
+		System.out.println(idadeNoAnoQueVem);
+	}
+
+}
