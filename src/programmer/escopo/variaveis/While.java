0bf66776fd40e8064347420c73c0ca1a6158eae5
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-29 15:54:26 -0300

    update class while, espaçamentos

--- src/programmer/escopo/variaveis/While.java
+++ src/programmer/escopo/variaveis/While.java
@@ -3,11 +3,13 @@ package programmer.escopo.variaveis;
 public class While {
 	
 	public static void main(String[]args){
+		
 		int idade = 15;
 		while (idade < 18){
 			System.out.println(idade);
 			idade++;
 		}
+		
 	}
 
 }
