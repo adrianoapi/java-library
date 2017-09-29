4706827dbb8cbde93efbdfc3a2a24ef67181ab32
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-29 15:50:20 -0300

    add class while

---
+++ src/programmer/escopo/variaveis/While.java
@@ -0,0 +1,13 @@
+package programmer.escopo.variaveis;
+
+public class While {
+	
+	public static void main(String[]args){
+		int idade = 15;
+		while (idade < 18){
+			System.out.println(idade);
+			idade++;
+		}
+	}
+
+}
