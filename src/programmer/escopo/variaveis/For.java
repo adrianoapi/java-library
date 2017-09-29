c2e01c130baf7ba0c201016d924ec53ea719044e
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-29 15:54:05 -0300

    add class For

---
+++ src/programmer/escopo/variaveis/For.java
@@ -0,0 +1,12 @@
+package programmer.escopo.variaveis;
+
+public class For {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Olá " + i);
+		}
+
+	}
+}
