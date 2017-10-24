03de35d7fd98c58f9e373a75ddada28bc1696c50
adrianometalslug <sdcomputadores@gmail.com>
2017-10-24 15:10:04 -0200

    add exemplo 2 foreach

--- src/programer/escopo/vetores/ForEach.java
+++ src/programer/escopo/vetores/ForEach.java
@@ -15,14 +15,41 @@ public class ForEach {
 			notas[i] = random.nextInt(10);
 		}
 
-//		for (int i = 0; i < notas.length; i++) {
-//			System.out.println(notas[i]);
-//		}
+		// for (int i = 0; i < notas.length; i++) {
+		// System.out.println(notas[i]);
+		// }
 
 		// usando forEach
-		for(int nota: notas){
+		for (int nota : notas) {
 			System.out.println(nota);
 		}
+
+		System.out.println("\n\nSeugndo exemplo:\n");
+
+		double[][] notasAlunos = new double[3][4];
+
+		notasAlunos[0][0] = 10;
+		notasAlunos[0][1] = 7;
+		notasAlunos[0][2] = 9;
+		notasAlunos[0][3] = 9.5;
+
+		notasAlunos[1][0] = 9;
+		notasAlunos[1][1] = 8;
+		notasAlunos[1][2] = 7;
+		notasAlunos[1][3] = 9;
+
+		notasAlunos[2][0] = 8;
+		notasAlunos[2][1] = 9;
+		notasAlunos[2][2] = 10;
+		notasAlunos[2][3] = 7;
+
+		for (double[] notasAluno : notasAlunos) {
+			for (double nota : notasAluno) {
+				System.out.println(nota + " " + nota);
+			}
+			System.out.println("\n");
+		}
+
 	}
 
 }
