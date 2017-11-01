efa6084837c2107340e3161a0a0075c7635df82a
adrianometalslug <sdcomputadores@gmail.com>
2017-11-01 16:15:48 -0200

    add estrutura básica class JogoDaVelha

---
+++ src/programer/escopo/vetores/JogoDaVelha.java
@@ -0,0 +1,24 @@
+package programer.escopo.vetores;
+
+import java.util.Scanner;
+
+public class JogoDaVelha {
+	
+	public static void main(String[]args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		char[][] jogoVelha = new char[3][3];
+		
+		System.out.println("Jogador 1 = X");
+		System.out.println("Jogador 2 = O");
+		
+		boolean ganhou = false;
+		
+		while(!ganhou){
+			
+		}
+		
+	}
+
+}
